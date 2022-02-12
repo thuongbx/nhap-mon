3eb07883719eb03f2907c489bb5e40da6abee706
Thuong Bui Xuan <xuanthuong103@gmail.com>
2022-02-12 03:45:00 -0800

    Thêm chức năng tính tuổi

--- NhapMon/Program.cs
+++ NhapMon/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.Write("Nhap nam sinh: ");
+var namSinh = Console.ReadLine();
+var namSinhSo = Int32.Parse(namSinh);
+
+Console.WriteLine("Tuoi cua ban la " + (2022 - namSinhSo));
